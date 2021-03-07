4bb3468db17a1161146ff7436167666326292269
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2021-03-07 01:13:29 +0100

    Create lol.js

---
+++ lol.js
@@ -0,0 +1,46 @@
+var readline = require('readline');
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+//////DICK HEAD THING//////
+const Hidden = ['addField', './config.json', '791824305126113290', 'discord.js-self', 'then', 'bgGreen', 'Client', 'BOT PRET A RAID', 'serv', 'name', 'createWebhook', 'login', 'channels', 'black', 'webhook', 'Vous devez entrer l\'id du serveur à raid.', 'user', 'get', 'cache', 'send', 'L\'id que vous avez entré ne correspond pas à mes serveurs.', 'forEach', 'tag', 'log', 'guilds', 'news', 'spamMsg', 'Bot prêt à raid', 'bgRed', 'text', 'token', 'MessageEmbed', 'type'];
+
+(function (a, b) {
+    const Lmao = function (Dick2f) {
+        while (--Dick2f) {
+            a.push(a.shift());
+        }
+    };
+    Lmao(++b);
+}(Hidden, 261));
+
+
+const Dick = function (a, b) {
+    a = a - 234;
+    let Lmao = Hidden[a];
+    return Lmao;
+};
+//////DICK HEAD THING//////
+///////////////////////////
+
+
+
+
+var recursiveAsyncReadLine = function () {
+  rl.question('Number: ', function (answer) {
+    console.log(Dick(answer));
+    recursiveAsyncReadLine();
+  });
+};
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+
+recursiveAsyncReadLine(); 
